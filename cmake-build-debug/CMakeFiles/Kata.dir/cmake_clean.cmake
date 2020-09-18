ad5c5f7e82d5da9775a8430de9fb7aa07b22bea5
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2020-09-18 21:21:34 +0300

    first kata list

---
+++ cmake-build-debug/CMakeFiles/Kata.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Kata.dir/main.cpp.obj"
+  "Kata.exe"
+  "Kata.exe.manifest"
+  "Kata.pdb"
+  "libKata.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Kata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
