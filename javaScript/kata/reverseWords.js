432ab1564435a1e573941d2bbab5b6606ec396ad
SyntaxErrorLineNULL <cyberorange16@gmail.com>
2021-08-17 22:31:33 +0300

    solution kata

---
+++ javaScript/kata/reverseWords.js
@@ -0,0 +1,10 @@
+/**
+ * Author: SyntaxErrorLineNULL
+ */
+
+'use strict';
+
+// Kata: https://www.codewars.com/kata/51c8991dee245d7ddf00000e/train/javascript
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
+
+module.exports = {reverseWords};
